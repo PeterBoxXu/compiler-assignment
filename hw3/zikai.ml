4b5a48277fae5f68f7acff41c79677774ab863b8
PeterXu <peterboxxu@gmail.com>
2023-10-30 15:31:42 +0100

    Added student tests to repo without integrating into test suite.

---
+++ hw3/zikai.ml
@@ -0,0 +1,7 @@
+let prefix = Test_config.global_prefix ^ "./compiler-design-eth-tests/03/zikai/"
+
+let zikai_tests = [
+    prefix ^ "nested_structs.ll", 16L
+  ; prefix ^ "nested_structs2.ll", 8L
+  ; prefix ^ "nested_structs3.ll", 127L
+]
